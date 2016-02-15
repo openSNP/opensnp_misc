aff7fb1d4bfb383e8fd43b6d15ed4cca576e4c7d
Bastian Greshake <bgreshake@googlemail.com>
2016-02-15 11:57:58 +0100

    new graphs

--- plotUsers.r
+++ plotUsers.r
@@ -1,41 +1,7 @@
-Sys.setlocale("LC_TIME","en_US.utf8") # use US time format
+library(ggplot2)
+library(ggthemes)
 
-# first, load the data
-users <- read.table("users.txt", sep="\t")
-# transform strings into proper time format
-users$V2 <- strptime(users$V2, format="%d.%m.%Y %H:%M")
-
-# old school plotting
-# plot x-axis manually later
-png("users.png")
-plot(users$V2, users$V1, type="l", ylab="Amount of user-accounts",xlab="Time", xaxt="n")
-dmin <- min(users$V2)
-dmax <- max(users$V2)
-dminjan <- as.POSIXct(format(dmin, "%Y-1-1"))
-seqYear <- seq(dminjan, by = "1 month", to = dmax + (86400 * 365)) # some padding to the x-axis
-# new x-axis
-axis(1, at = seqYear, labels = format(seqYear, "%b-%y"), las = 1)
-dev.off()
-
-library("xkcd") # loads ggplot too
-# ggplot plotting
-users <- as.data.frame(users)
-a <- ggplot(data=users, aes(x=V2, y=V1))
-a <- a + geom_line()
-a <- a + geom_point()
-a <- a + xlab("Time") + ylab("Number user accounts")
-ggsave("users2.png", a)
-
-# xkcd style plotting
-# randomness is used in xkcd graphs, set seed for reproducibility
-set.seed(123)
-xrange <- range(users$V2)
-#yrange <- range(users$V1)
-# this currently goes to 3800, I want 4000
-yrange <- c(0, 4000)
-p <- ggplot() + 
-        geom_point(aes(x=as.Date(V2),y=V1), data=users) + 
-        xkcdaxis(xrange,yrange) +
-        ylab("Number of user accounts") +
-        xlab("Time")
-ggsave("users3.png",p)
+user <- read.csv(file="usernumber.csv",sep="\t",head=F)
+user$date <- as.POSIXct(as.character(user$V2),format="%d.%m.%Y %H:%M")
+ggplot(user,aes(user$date,user$V1)) + geom_smooth() + theme_light() + scale_x_datetime("Date") + scale_y_continuous("# of users")
+ggsave("users.pdf")
--- user_export.rb
+++ user_export.rb
@@ -1,6 +1,6 @@
 counter = 0
 
-File.open("/tmp/users.txt","w"){ |file|
+File.open("/home/app/snpr/public/data/usernumber.csv","w"){ |file|
   User.find_each do |u|
     counter += 1
     file.write("#{counter}\t#{u.created_at}\n")
